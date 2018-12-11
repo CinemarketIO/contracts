882d303ea21981e36a4e98ae4d55b52af0f8d125
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-12-11 17:28:48 +0100

    Keep script running

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,6 @@ then
     concurrently 'npm run watch' 'ganache-cli --networkId 15 --gasLimit=50000000 -h=0.0.0.0 -p 8545 -m candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
 else
     npm run migrate -- --network=${NETWORK}
+    # keep container running for volumes
+    tail -f /dev/null
 fi
