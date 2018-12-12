45a23c9f2c40b3fc89cc5ce244e77b767e89b6de
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2018-12-12 09:00:30 +0100

    Test single token minting

--- test/coalaipright.test.js
+++ test/coalaipright.test.js
@@ -4,7 +4,23 @@ const truffleAssert = require("truffle-assertions");
 var CoalaIPRight = artifacts.require("./build/contracts/COALAIPRight");
 
 contract("COALA IP Right", function(accounts) {
-  it("should create a new token contract and a mint a token", async () => {
+  it("should create a new token", async () => {
+    const token = await CoalaIPRight.new("CoalaIP Right", "CIPR");
+    const tokenURI = "https://example.com/0";
+    const tokenURIs = [tokenURI];
+    const addresses = [accounts[0]];
+
+    const res = await token.mint(addresses.join(","), tokenURIs.join(","), {
+      from: accounts[0]
+    });
+
+    assert.equal(await token.balanceOf(addresses[0]), 1);
+    assert.equal(await token.tokenURI(0), "https://example.com/0");
+    truffleAssert.eventEmitted(res, "Transfer", ev => {
+      return ev._to === addresses[0];
+    });
+  });
+  it("should create 10 new tokens", async () => {
     const token = await CoalaIPRight.new("CoalaIP Right", "CIPR");
     const tokenURIs = [];
     for (let i = 0; i < 10; i++) {
